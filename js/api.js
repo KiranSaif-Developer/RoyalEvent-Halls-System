8b2a31e7b6b78ad188a9062d80056db06f72a912
Kiran Saif <kiransaif.gul@gmail.com>
2026-06-26 12:45:22 +0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://royalevent-halls-system-production.up.railway.app';
 export const getData = async (endpoint, id = null) => {
     const url = id ? `${API_BASE_URL}/${endpoint}/${id}` : `${API_BASE_URL}/${endpoint}`;
     try {
